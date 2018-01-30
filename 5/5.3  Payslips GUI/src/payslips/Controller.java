54a4285a7e23268a57f435372a01a508a3f2dfc0
Unknown <matamanthemaster@gmail.com>
2018-01-30 12:00:50 +0000

    5.3 GUI Start

---
+++ 5/5.3  Payslips GUI/src/payslips/Controller.java
@@ -0,0 +1,16 @@
+package payslips;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML private TextField txtHours;
+    @FXML private TextField txtRate;
+    @FXML private TextField txtDispPay;
+
+    void textChanged()
+    {
+
+    }
+}
