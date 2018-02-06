759cf0d0bb6621c71794316a01f6210106b766a4
Unknown <matamanthemaster@gmail.com>
2018-02-06 09:56:21 +0000

    Finished 5.3 CSS, sizing and Documentation

--- 5/5.3  Payslips GUI/src/payslips/Main.java
+++ 5/5.3  Payslips GUI/src/payslips/Main.java
@@ -14,13 +14,17 @@ public class Main extends Application {
         //get the contents of Main.fxml to append to the scene
         Parent root = FXMLLoader.load(getClass().getResource("payslips.fxml"));
 
-        //set the scene with the contentsof Main.fxml and default size.
-        Scene sceneMain = new Scene(root, 500, 200);
+        //set the scene with the contents of Main.fxml and default size.
+        Scene sceneMain = new Scene(root, 500, 250);
 
 
         //get the css files and add it to the scene's stylesheets.
         sceneMain.getStylesheets().add(getClass().getResource("css/style.css").toExternalForm());
         sceneMain.getStylesheets().add(getClass().getResource("css/colour.css").toExternalForm());
+
+        //set stage minimum sizes
+        stageMain.setMinWidth(300);
+        stageMain.setMinHeight(150);
 
         //set title, scene and show the stage.
         stageMain.setTitle("5.3 Payslips");
